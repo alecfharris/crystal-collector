6c7401453739bc7a015dc54a3046a6ea011cbe64
Alec Harris <alecfharris@gmail.com>
2018-10-18 11:15:54 -0500

    updated readme and refactored code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# unit-4-game
+# Crystals Collector
+
+*The crystals needed to have a random value and there needed to be a random goal. In order to get to that random value,
+the crystals could be clicked, adding to the score. A matching score results in a win.
+
+*This was accomplished by using Javascript functions to randomly generate a number for each crystal
+and for the total. 
+*Once the number was determined, JQuery was used to make on click events and to increase the score
+by the number clicked.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,67 +7,61 @@ var blueNumber = 0;
 var redNumber = 0;
 var purpleNumber = 0;
 
-$(document).ready(function(){
-    // Make a function that will generate a random number and set that to the current goal number
+$(document).ready(function () {
+    // Generates a random number and sets that to the current goal number
     function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     function setNumbers() {
-        currentGoalNumber = getRndInteger(19,120);
-        greenNumber = getRndInteger(1,12);
-        blueNumber = getRndInteger(1,12);
-        redNumber = getRndInteger(1,12);
-        purpleNumber = getRndInteger(1,12);
+        currentGoalNumber = getRndInteger(19, 120);
+        greenNumber = getRndInteger(1, 12);
+        blueNumber = getRndInteger(1, 12);
+        redNumber = getRndInteger(1, 12);
+        purpleNumber = getRndInteger(1, 12);
         totalScoreNumber = 0;
 
         updateScore();
 
         $("#current-goal").text("Current Goal: " + currentGoalNumber);
-        
-        // console.log("Current Goal: " + currentGoalNumber);
-        // console.log("Green: " + greenNumber);
-        // console.log("Blue: " + blueNumber);
-        // console.log("Red: " + redNumber);
-        // console.log("Purple: " + purpleNumber);
     }
     setNumbers();
-    // Make a function that updates the totalScore
-    function updateScore(){
+    // Updates the total score
+    function updateScore() {
         $("#total-score-number").text(totalScoreNumber);
     }
-    // Make a function to check if the total score is the right number
-    function checkScore(){
-        if(totalScoreNumber === currentGoalNumber && currentGoalNumber != 0){
+    // Checks if the total score is the right number
+    function checkScore() {
+        if (totalScoreNumber === currentGoalNumber && currentGoalNumber != 0) {
             wins++;
             $("#wins").text("Wins: " + wins);
             setNumbers();
         }
-        else if(totalScoreNumber > currentGoalNumber){
+        else if (totalScoreNumber > currentGoalNumber) {
             losses++;
             $("#losses").text("Losses: " + losses);
             setNumbers();
         }
     }
-    // Make a function that updates the score upon clicking a crystal
-    $("#green-crystal").on("click", function() {
+    // Updates the score upon clicking a crystal
+    $("#green-crystal").on("click", function () {
         totalScoreNumber += greenNumber;
         updateScore();
         checkScore();
     });
 
-    $("#blue-crystal").on("click", function() {
+    $("#blue-crystal").on("click", function () {
         totalScoreNumber += blueNumber;
         updateScore();
         checkScore();
     });
 
-    $("#red-crystal").on("click", function() {
+    $("#red-crystal").on("click", function () {
         totalScoreNumber += redNumber;
         updateScore();
         checkScore();
     });
 
-    $("#purple-crystal").on("click", function() {
+    $("#purple-crystal").on("click", function () {
         totalScoreNumber += purpleNumber;
         updateScore();
         checkScore();
